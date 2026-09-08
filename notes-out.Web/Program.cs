80c51cb31bbb8c4d3e5f50799ab58462f16263cc
Leandro De la cruz <leandrocitygear@icloud.com>
2026-09-08 15:29:00 -0400

    test

--- notes-out.Web/Program.cs
+++ notes-out.Web/Program.cs
@@ -3,40 +3,35 @@ using notes_out.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
-//builder.AddRedisOutputCache("cache");
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddHttpClient<WeatherApiClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        var apiUrl = builder.Configuration["API_BASE_URL"]
+{
+    var apiUrl = builder.Configuration["API_BASE_URL"]
         ?? "https+http://apiservice";
 
-        client.BaseAddress = new(apiUrl);
-    });
+    client.BaseAddress = new(apiUrl);
+});
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAntiforgery();
 
-//app.UseOutputCache();
-
-app.UseStaticFiles();
+app.MapStaticAssets();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
